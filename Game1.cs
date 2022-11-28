f842b6fb55ecc5448fef291ed769e54c6e86b7ac
esoccoli <soccoli2015@gmail.com>
2022-11-27 20:29:48 -0500

    Added comments from devcade game template

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Devcade;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +10,9 @@ namespace SpaceInvader
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        /// <summary>
+        /// Game constructor
+        /// </summary>
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -16,35 +20,75 @@ namespace SpaceInvader
             IsMouseVisible = true;
         }
 
+        /// <summary>
+        /// Does any setup prior to the first frame that doesn't need loaded content.
+        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
 
+            Input.Initialize(); // Sets up the input library
+
+            // Set window size if running debug (in release it will be fullscreen)
+            #region
+#if DEBUG
+            _graphics.PreferredBackBufferWidth = 420;
+            _graphics.PreferredBackBufferHeight = 980;
+            _graphics.ApplyChanges();
+#else
+			_graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+			_graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
+			_graphics.ApplyChanges();
+#endif
+            #endregion
+
             base.Initialize();
         }
+
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            // ex.
+            // texture = Content.Load<Texture2D>("fileNameWithoutExtention");
         }
 
+        /// <summary>
+        /// Your main update loop. This runs once every frame, over and over.
+        /// </summary>
+        /// <param name="gameTime">This is the gameTime object you can use to get the time since last frame</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Input.Update(); // Updates the state of the input library
+
+            // Exit when both menu buttons are pressed (or escape for keyboard debuging)
+            // You can change this but it is suggested to keep the keybind of both menu
+            // buttons at once for gracefull exit.
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) ||
+                (Input.GetButton(1, Input.ArcadeButtons.Menu) &&
+                Input.GetButton(2, Input.ArcadeButtons.Menu)))
+            {
                 Exit();
+            }
 
             // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// Your main draw loop. This runs once every frame, over and over.
+        /// </summary>
+        /// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
+            _spriteBatch.Begin();
             // TODO: Add your drawing code here
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
