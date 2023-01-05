a9be422250895b09e1eb21e726da706b5025a946
Ella Soccoli <112343747+esoccoli@users.noreply.github.com>
2023-01-05 11:33:40 -0500

    Laser constructors

--- Laser.cs
+++ Laser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,51 @@ namespace SpaceInvader
 {
     internal class Laser
     {
+        private int X { get; set; }
+        private int Y { get; set; }
+        private int Width { get; set; }
+        private int Height { get; set; }
+        private Rectangle Beam { get; set; }
+        private Color Color { get; set; }
+
+        public Laser(int x, int y, int width, int height, Color color)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Beam = new Rectangle(X, Y, Width, Height);
+            Color = color;
+        }
+
+        public Laser(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Beam = new Rectangle(X, Y, Width, Height);
+            Color = Color.Green;
+        }
+
+        public Laser(Rectangle beam, Color color)
+        {
+            Beam = beam;
+            X = beam.X;
+            Y = beam.Y;
+            Width = beam.Width;
+            Height = beam.Height;
+            Color = color;
+        }
+
+        public Laser(Rectangle beam)
+        {
+            Beam = beam;
+            X = beam.X;
+            Y = beam.Y;
+            Width = beam.Width;
+            Height = beam.Height;
+            Color = Color.Green;
+        }
     }
 }
